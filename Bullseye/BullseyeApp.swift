9c41714e5caef0e6f79b29c4111b5b41c2cf291e
JROD-21 <joseph.f.rodas@gmail.com>
2022-09-25 13:18:40 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Developer J-ROD on 9/25/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
